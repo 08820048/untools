88a7932b7dd2ce74087c435ee07a1863526d6908
s4:D_t0y <roccowu949@gmail.com>
2024-05-10 23:55:53 -0700

    Batch works with new cli

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 use std::fs;
 
-
-use utils::{is_camel_or_pascal_case, starts_with_digit};
 use console::style;
+use utils::{is_camel_or_pascal_case, starts_with_digit};
 
 mod utils;
 /// Converts a camelCase or PascalCase string to snake_case.
@@ -15,20 +14,31 @@ mod utils;
 /// # Arguments
 ///
 /// * `name` - A string slice that holds the variable name to be converted.
-/// * `is_constant` - A boolean flag indicating if the output should be in uppercase.
+/// * `is_constant` - A boolean flag indicating if the output should be in all caps.
 ///
 /// # Returns
 ///
 /// A String containing the snake_case version of the input variable name.
+/// 
+/// # Usage
+///
+/// This converts the the variable "testVariable" from camelcase to snakecase in all caps.
+/// 
+/// ``` 
+/// assert_eq!(camel_to_snake("testVariable", true), "TEST_VARIABLE")
+/// ```
 ///
 pub fn camel_to_snake(name: &str, is_constant: bool) -> String {
     if name.is_empty() {
-        println!("{}",style("Input string is empty. Please provide a valid variable name.").color256(208));
+        println!(
+            "{}",
+            style("Input string is empty. Please provide a valid variable name.").color256(208)
+        );
         return String::new();
     }
 
     if !starts_with_digit(name) {
-        println!("{}",style("Input string is a digit.").color256(208));
+        println!("{}", style("Input string is a digit.").color256(208));
         return String::new();
     }
 
@@ -37,7 +47,11 @@ pub fn camel_to_snake(name: &str, is_constant: bool) -> String {
     }
 
     if !is_camel_or_pascal_case(name) {
-        println!("{}",style("is not in camelCase format. Please provide a valid camelCase variable name.").color256(208));
+        println!(
+            "{}",
+            style("is not in camelCase format. Please provide a valid camelCase variable name.")
+                .color256(208)
+        );
         return String::new();
     }
 
@@ -57,26 +71,28 @@ pub fn camel_to_snake(name: &str, is_constant: bool) -> String {
     result
 }
 
-
 /// Batch convert variable names
-/// 
+///
 /// This function reads variable name data from the specified input file, converts them to the specified naming convention (camelCase or SCREAMING_SNAKE_CASE),
 /// and then writes the converted results to the specified output file.
-/// 
+///
 /// # Arguments
 /// - `ifile`: The path to the input file
 /// - `ofile`: The path to the output file
 /// - `is_constant`: Whether to convert to SCREAMING_SNAKE_CASE, `true` to convert to SCREAMING_SNAKE_CASE, `false` to convert to camelCase
-/// 
+///
 /// # Example
-/// 
+///
 /// ```rust
 /// untools::batch_convert("input.txt", "output.txt", true);
 /// ```
 pub fn batch_convert(ifile: &str, ofile: &str, is_constant: bool) {
     let contents = fs::read_to_string(ifile).expect("Unable to read file.");
 
-    let converted_names: Vec<String> = contents.lines().map(|line| camel_to_snake(line.trim(), is_constant)).collect();
+    let converted_names: Vec<String> = contents
+        .lines()
+        .map(|line| camel_to_snake(line.trim(), is_constant))
+        .collect();
 
     let output_content = converted_names.join("\n");
     fs::write(ofile, output_content).expect("Unable to write file.");
@@ -95,10 +111,12 @@ pub fn batch_convert(ifile: &str, ofile: &str, is_constant: bool) {
 ///
 /// * A String containing the camelCase representation of the input string.
 ///
-pub fn snake_to_camel(name: &str, mut to_lower_camel: bool) -> String {
+pub fn snake_to_camel(name: &str, upper_case_camel: bool) -> String {
     let mut camel_case = String::new();
     let mut capitalize_next = true;
 
+    let mut to_lower_camel = !upper_case_camel;
+    
     if name.is_empty() {
         return String::from("Input string is empty. Please provide a valid variable name.");
     }
@@ -124,6 +142,5 @@ pub fn snake_to_camel(name: &str, mut to_lower_camel: bool) -> String {
             }
         }
     }
-camel_case
+    camel_case
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,64 @@
-
-
 //! # uutols crate
 //! A simple and user-friendly underscore variable naming tool
+use std::io;
+
 /// Main function to demonstrate the usage of the ctsc_utils crate.
 ///
 /// This function calls the `camel_to_snake_case` function from the `ctsc_utils` crate
 /// to convert the variable name "myVariableName" from camelCase to snake_case format,
 /// and then prints the result to the console.
 //mod utils;
-use untools::{camel_to_snake,batch_convert,snake_to_camel};
-fn main() {
-    println!("{}", camel_to_snake("121",true));
-    let input_file = "input.txt";
-    let output_file = "output.txt";
-    //batch_convert(input_file, output_file,true);
-    //println!("res :{}",snake_to_camel("_case_test",false));
+use untools::{camel_to_snake, snake_to_camel, batch_convert};
+
+use clap::{Parser, Args};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+#[command(arg_required_else_help(true))]
+struct Cli {
+    input: String,
+    
+    #[arg(long, short = 'c')]
+    is_constant: bool,
+    
+    #[command(flatten)]
+    conversion_type: ConversionType,
+}
+
+#[derive(Args, Debug)]
+#[group(required = true, multiple = false)]
+struct ConversionType {
+    #[arg(long)]
+    camel_to_snake: bool,
+    
+    #[arg(long)]
+    snake_to_camel: bool,
+    
+    #[arg(long, value_name = "OUTPUT_FILE")]
+    batch: Option<String>,
+}
+
+
+fn main() -> io::Result<()> {
+    let args = Cli::parse();
+    
+    let input = args.input;
+    
+    let is_constant = args.is_constant;
+    
+    let conversion_type = args.conversion_type;
+    let (cts, stc, b) = (conversion_type.camel_to_snake, conversion_type.snake_to_camel, conversion_type.batch);
+    
+    let _result;
+    match (cts, stc, b) {
+        (true, _, _) => { _result = camel_to_snake(input.as_str(), is_constant); }
+        
+        (_, true, _) => { _result = snake_to_camel(input.as_str(), is_constant) },
+        
+        (_, _, Some(output_file)) => { batch_convert(input.as_str(), output_file.as_str(), is_constant) },
+        
+        _ => { unreachable!() }
+    }
+    
+    Ok(())
 }
